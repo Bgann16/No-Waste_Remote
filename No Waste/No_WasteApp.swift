50a344042438e49452eed484f99419f50e88b09e
Ben Gann <bengann414@gmail.com>
2023-08-31 19:44:45 -0500

    Initial Commit

---
+++ No Waste/No_WasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  No_WasteApp.swift
+//  No Waste
+//
+//  Created by ben gann on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct No_WasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
